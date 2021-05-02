b1db769470bc46e8fefd190b6618f745ca4bff55
rubciv <67111645+rubciv@users.noreply.github.com>
2021-05-02 20:42:53 -0300

    prueba1

--- ejercicios_practica/ejercicio_5.py
+++ ejercicios_practica/ejercicio_5.py
@@ -20,3 +20,7 @@ palabra_2 = str(input())
 # De la segunda palabra tome las primeras dos letras, utilice el operador :
 # Formar una nueva palabra con los recortes solicitados
 # Imprima en pantalla los resultados
+primera = palabra_1[0:3]
+segunda = palabra_2[0:3]
+completa = primera + segunda
+print (completa)
